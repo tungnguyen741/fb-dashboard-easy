f6b02bff84435f06ca7e958bc692ac470d1011ad
tungnt16 <tungnt16@vng.com>
2022-12-10 14:59:32 +0700

    init project

--- webpack.config.js
+++ webpack.config.js
@@ -200,43 +200,43 @@ module.exports = (env, argv) => {
                 chunks: 'all',
             },
             minimizer: [
-                new ImageMinimizerPlugin({
-                    minimizer: {
-                        implementation: ImageMinimizerPlugin.imageminMinify,
-                        options: {
-                            // Lossless optimization with custom option
-                            // Feel free to experiment with options for better result for you
-                            plugins: [
-                                ["gifsicle", { interlaced: true }],
-                                ["jpegtran", { progressive: true }],
-                                ["optipng", { optimizationLevel: 7 }],
-                                // Svgo configuration here https://github.com/svg/svgo#configuration
-                                [
-                                    "svgo",
-                                    {
-                                        plugins: [
-                                            {
-                                                name: "preset-default",
-                                                params: {
-                                                    overrides: {
-                                                        removeViewBox: false,
-                                                        addAttributesToSVGElement: {
-                                                            params: {
-                                                                attributes: [
-                                                                    { xmlns: "http://www.w3.org/2000/svg" },
-                                                                ],
-                                                            },
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        ],
-                                    },
-                                ],
-                            ],
-                        },
-                    },
-                }),
+                // new ImageMinimizerPlugin({
+                //     minimizer: {
+                //         implementation: ImageMinimizerPlugin.imageminMinify,
+                //         options: {
+                //             // Lossless optimization with custom option
+                //             // Feel free to experiment with options for better result for you
+                //             plugins: [
+                //                 ["gifsicle", { interlaced: true }],
+                //                 ["jpegtran", { progressive: true }],
+                //                 ["optipng", { optimizationLevel: 7 }],
+                //                 // Svgo configuration here https://github.com/svg/svgo#configuration
+                //                 [
+                //                     "svgo",
+                //                     {
+                //                         plugins: [
+                //                             {
+                //                                 name: "preset-default",
+                //                                 params: {
+                //                                     overrides: {
+                //                                         removeViewBox: false,
+                //                                         addAttributesToSVGElement: {
+                //                                             params: {
+                //                                                 attributes: [
+                //                                                     { xmlns: "http://www.w3.org/2000/svg" },
+                //                                                 ],
+                //                                             },
+                //                                         },
+                //                                     },
+                //                                 },
+                //                             },
+                //                         ],
+                //                     },
+                //                 ],
+                //             ],
+                //         },
+                //     },
+                // }),
             ]
         },
     };
